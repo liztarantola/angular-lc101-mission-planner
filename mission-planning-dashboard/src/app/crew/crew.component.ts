e935082ea5253f0d75f26f3d7621801cec7ad8d1
Elizabeth Tarantola <elizabethtarantola@Elizabeths-MacBook-Pro.local>
2021-03-29 19:36:56 -0500

    studio1 with bonusmission

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -4,9 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-crew',
   templateUrl: './crew.component.html',
   styleUrls: ['./crew.component.css']
+  
 })
 export class CrewComponent implements OnInit {
-
+  crew: string[] = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"]
   constructor() { }
 
   ngOnInit() {
